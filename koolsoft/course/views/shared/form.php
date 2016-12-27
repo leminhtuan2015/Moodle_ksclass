5bb6beadd1a2bf876ca23c327a9fdb7f46240951
leminhtuan <leminhtuan2015@gmail.com>
2016-12-27 12:59:40 +0700

    free_course

--- koolsoft/course/views/shared/form.php
+++ koolsoft/course/views/shared/form.php
@@ -24,7 +24,6 @@
                                 } else {
                                     echo "<option value='$key'> $categoryName </option>";
                                 }
-
                             }
                             ?>
                         </select>
