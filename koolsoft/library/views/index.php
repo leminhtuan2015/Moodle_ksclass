b99d91c4bd57a3c96dda3b6dd7b531fa46c2a389
leminhtuan <leminhtuan2015@gmail.com>
2016-12-30 13:21:08 +0700

    library1_css

--- koolsoft/library/views/index.php
+++ koolsoft/library/views/index.php
@@ -1,3 +1,17 @@
+<style>
+    .nav-tabs { border-bottom: 2px solid #DDD; }
+    .nav-tabs > li.active > a, .nav-tabs > li.active > a:focus, .nav-tabs > li.active > a:hover { border-width: 0; }
+    .nav-tabs > li > a { border: none; color: #666; }
+    .nav-tabs > li.active > a, .nav-tabs > li > a:hover { border: none; color: #4285F4 !important; background: transparent; }
+    .nav-tabs > li > a::after { content: ""; background: #4285F4; height: 2px; position: absolute; width: 100%; left: 0px; bottom: -1px; transition: all 250ms ease 0s; transform: scale(0); }
+    .nav-tabs > li.active > a::after, .nav-tabs > li:hover > a::after { transform: scale(1); }
+    .tab-nav > li > a::after { background: #21527d none repeat scroll 0% 0%; color: #fff; }
+    .tab-pane { padding: 15px 0; }
+    .tab-content{padding:20px}
+
+    .card {background: #FFF none repeat scroll 0% 0%; box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.3); margin-bottom: 30px; }
+</style>
+
 <div class="container">
     <div class="row">
         <div class="col-md-12">
@@ -13,7 +27,7 @@
                         <?php require_once ("files/index.php");?>
                     </div>
                     <div role="tabpanel" class="tab-pane" id="profile">
-                        <?php require_once (__DIR__."/../../question_categories/index.php");?>
+<!--                        --><?php //require_once (__DIR__."/../../question_categories/index.php");?>
                     </div>
                 </div>
             </div>
