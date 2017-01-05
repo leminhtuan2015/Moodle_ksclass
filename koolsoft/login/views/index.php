87f1ba21cfcd83586df9dab3dd53e0d2aeaa709f
leminhtuan <leminhtuan2015@gmail.com>
2017-01-05 20:31:37 +0700

    login_function

---
+++ koolsoft/login/views/index.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <form action="/moodle/koolsoft/login/?action=login" method="post">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Username/Email address</label>
+            <input type="text" class="form-control" placeholder="Username/Enter email" name="username">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
