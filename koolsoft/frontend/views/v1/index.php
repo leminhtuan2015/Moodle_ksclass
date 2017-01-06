24a31361f8a5940fb229f59bf9bc8e192b9de1b3
leminhtuan <leminhtuan2015@gmail.com>
2017-01-06 16:05:35 +0700

    change_detail_ui

--- koolsoft/frontend/views/v1/index.php
+++ koolsoft/frontend/views/v1/index.php
@@ -6,6 +6,7 @@
 	.panelContent{
 		width: 90%;
 		margin: 0 auto;
+        margin-bottom: 20px;
 	}
 	.hearder-Home{
 		width: 100%;
@@ -29,10 +30,10 @@
         </a>
 		<div style="padding:30px 0px;">
 			<a class='iconFont-Home' href="" ><i class="fa fa-star-o" style="color:black;width:30px;"></i></a>
-			<h4 style="display:inline-block;">Lop yeu thich<a href="">Tat ca</a></h4>
+			<h4 style="display:inline-block;">Lop yeu thich<a href="" style="font-size: 0.8em;"> Tat ca</a></h4>
 		</div>
 
-        <div class="" style="width: 100%">
+        <div class="" style="width: 100%; overflow: hidden;">
             <?php
                 foreach ($myCourses as $course) {
                     include ("course_overview.php");
@@ -42,10 +43,10 @@
 
 		<div style="padding:30px 0px" style="margin-top:10px;">
 			<a style="font-size: 2.0em;" href="" ><i class="fa fa-user-plus" style="color:black;width:30px;"></i></a>
-			<h4 style="display:inline-block;">Lop cua toi<a href="">Tat ca</a></h4>
+			<h4 style="display:inline-block;">Lop cua toi<a href="" style="font-size: 0.8em;"> Tat ca</a></h4>
 		</div>
 
-        <div class="" style="width: 100%">
+        <div class="" style="width: 100%; overflow: hidden;">
             <?php
                 foreach ($myCourses as $course) {
                     include ("course_overview.php");
@@ -55,10 +56,10 @@
 
 		<div style="padding:30px 0px">
 			<a style="font-size: 2.0em;" href="" ><i class="fa fa-thumbs-o-up" style="color:black;width:30px;"></i></a>
-			<h4 style="display:inline-block;">Lop noi bat<a href="">Tat ca</a></h4>
+			<h4 style="display:inline-block;">Lop noi bat<a href="" style="font-size: 0.8em;"> Tat ca</a></h4>
 		</div>
 
-        <div class="" style="width: 100%">
+        <div class="" style="width: 100%; overflow: hidden;">
             <?php
             foreach ($courses as $course) {
                 if($course->sortorder == 1){ continue;}
