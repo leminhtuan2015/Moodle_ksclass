647678294560dab68308fc022959dc8eaed8baad
7buon <7uon@example.com>
2017-01-14 16:49:32 +0700

    minchange_active_link

--- koolsoft/course/views/v1/resources/chapter.js
+++ koolsoft/course/views/v1/resources/chapter.js
@@ -1,8 +1,16 @@
 $(".active_link_lecture_test").click(function(){
+	$(".active_link_lecture").removeClass("active_link_lecture_test_color");
 	$(".active_link_lecture_test").removeClass("active_link_lecture_test_color");
 	$(this).addClass("active_link_lecture_test_color");
+	// ACTIVE CURRENT LECTURE
+
+	parentId = $(this).attr("parent-id")
+	// alert(parentId)
+	$("#lecture_" + parentId).addClass("active_link_lecture_test_color");
+
 });
 $(".active_link_lecture").click(function(){
 	$(".active_link_lecture").removeClass("active_link_lecture_test_color");
+	$(".active_link_lecture_test").removeClass("active_link_lecture_test_color");
 	$(this).addClass("active_link_lecture_test_color");
 });
--- koolsoft/frontend/views/v1/course_overview.php
+++ koolsoft/frontend/views/v1/course_overview.php
@@ -2,7 +2,7 @@
     <a href="/moodle/koolsoft/course/?action=show&id=<?php echo $course->id ?>">
         <div style="border:1px solid black;border-radius:5px;">
             <div style="background-color:#EEEEEE;padding:5px;border-radius:5px;">
-                <h5 style="display:inline-block;" class="itemCategory-Home"><?php echo $course->fullname ?></h5>
+                <h5 style="display:inline-block;color:black;" class="itemCategory-Home"><?php echo $course->fullname ?></h5>
                 <i class="fa fa-star-o" style="color:black;width:30px;font-size: 2.0em;"></i>
             </div>
             <h5 style="color:#8BC77B;padding:0px 5px;"><?php echo $course->cost ?>free</h5>
