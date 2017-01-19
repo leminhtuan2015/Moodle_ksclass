13ef12027869661bcfe15b420ffaae26ecbed6a2
leminhtuan <leminhtuan2015@gmail.com>
2017-01-19 13:19:40 +0700

    view_pdf

---
+++ koolsoft/resources/javascript/tinymce/js/tinymce/plugins/iframe/content.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="/moodle/koolsoft/resources/css/font-awesome.min.css">
+<link rel="stylesheet" href="/moodle/koolsoft/resources/css/bootstrap.3.3.7.min.css">
+<link rel="stylesheet" href="/moodle/koolsoft/resources/css/bootstrap-datetimepicker.min.css">
+<link rel="stylesheet" href="/moodle/koolsoft/resources/css/select2.min.css">
+<link rel="stylesheet" href="/moodle/koolsoft/resources/css/jquery-ui.min.css">
+
+<div class="form-group">
+    <label>PDF link:</label>
+    <input type="text" id="pdf_link" name="pdf_link_input">
+</div>
